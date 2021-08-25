1caa46f0f3dd0e9b1c4f6910d75be4df92bab567
Hongtae Gim <hongtae0000.kdigital@gmail.com>
2021-08-25 17:45:26 +0900

    web programming, django basic

---
+++ aug24/cgi-server/cgi_client.py
@@ -0,0 +1,17 @@
+#!/Users/monsieur/opt/anaconda3/bin/python
+
+from urllib.request import urlopen
+from urllib.parse import urlencode
+
+
+url = "http://127.0.0.1:8888/cgi-server/cgi-bin/script.py"
+data = {
+    'name': '홍길동',
+    'email': 'HongGD@naver.com',
+    'url': 'http://www.naver.com',
+}
+encData = urlencode(data)
+postData = encData.encode('ascii')
+
+f = urlopen(url, postData)   # POST
+print(f.read().decode('cp949'))
